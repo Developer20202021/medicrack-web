48c0ab1bd4cb82e185769439db2f200ded8ab165
Developer20202021 <spectrum.courses.bd@gmail.com>
2025-12-22 12:54:43 +0600

    done

--- src/Documentation/DocumentationViewerPage.js
+++ src/Documentation/DocumentationViewerPage.js
@@ -129,8 +129,6 @@ const DocumentationViewerPage = () => {
     
     const FlaskConical = (props) => React.createElement('svg', {...props, xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24", fill: "none", stroke: "currentColor", strokeWidth: "2", strokeLinecap: "round", strokeLinejoin: "round"}, React.createElement('path', {d: "M10 2v7.527a2 2 0 0 1-.211.896L4.72 20.55a1 1 0 0 0 .9 1.45h12.76a1 1 0 0 0 .9-1.45l-5.069-10.127A2 2 0 0 1 14 9.527V2"}), React.createElement('path', {d: "M8.5 2h7"}), React.createElement('path', {d: "M7 16h10"}));
 
-    const ChevronRight = (props) => React.createElement('svg', {...props, xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24", fill: "none", stroke: "currentColor", strokeWidth: "2", strokeLinecap: "round", strokeLinejoin: "round"}, React.createElement('path', {d: "m9 18 6-6-6-6"}));
-    
     const ChevronDown = (props) => React.createElement('svg', {...props, xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24", fill: "none", stroke: "currentColor", strokeWidth: "2", strokeLinecap: "round", strokeLinejoin: "round"}, React.createElement('path', {d: "m6 9 6 6 6-6"}));
     
     const Droplet = (props) => React.createElement('svg', {...props, xmlns: "http://www.w3.org/2000/svg", width: "24", height: "24", viewBox: "0 0 24 24", fill: "none", stroke: "currentColor", strokeWidth: "2", strokeLinecap: "round", strokeLinejoin: "round"}, React.createElement('path', {d: "M12 2.69l5.66 5.66a8 8 0 1 1-11.31 0z"}));
